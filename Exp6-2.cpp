f34c1ae0058b3ce0dbc7771d11a5f4e242d354c2
suryapratap1121 <thakur.surya1303@gmail.com>
2025-07-31 12:19:38 +0530

    Create Exp6-2.cpp

---
+++ Exp6-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<5;i++){
+       
+            cout<<i+1<<"."<< "SIT"<<endl;
+       
+    }
+  return 0;
+}
+
+//output
+//1.SIT
+//2.SIT
+//3.SIT
+//4.SIT
+//5.SIT
