bb3b20b927e5d82662a6ea0bdacfba2e641b5fd7
suryapratap1121 <thakur.surya1303@gmail.com>
2025-07-31 12:19:08 +0530

    Create Exp1.cpp

---
+++ Exp1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=1;i<=10;i++){
+        if ((i%2)==0){
+            cout<<i<<endl;
+        }
+    }
+ return 0;  
+}
+
+// output 
+//2
+//4
+//6
+//8
+//10
