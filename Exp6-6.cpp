8e636115233c4e33809ce7277050bec1ac07e172
suryapratap1121 <thakur.surya1303@gmail.com>
2025-07-31 12:23:43 +0530

    Create Exp6-6.cpp

---
+++ Exp6-6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 6; i++) {
+        for (int j = 1; j <= 6 - i; j++) {
+            cout << "  ";
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*
+output:
+          * 
+        * * 
+      * * * 
+    * * * * 
+  * * * * * 
+* * * * * *
+    */
